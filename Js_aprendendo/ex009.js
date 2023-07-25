02721e7fa4b6f76b6662faf178b64ed97281e3d3
Tiuted <tilted2266@gmail.com>
2023-07-24 22:51:44 -0300

    isso ai

---
+++ Js_aprendendo/ex009.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km`)
+if (vel > 60) { //Condição simples
+    console.log('vc ultrapassou a velocidade permitida. MULTADO!')
+}
+
+
+console.log('Dirija sempre usando cinto de segurança')
